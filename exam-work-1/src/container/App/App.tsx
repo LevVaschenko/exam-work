a2e058737bb70169126231e0dcb7935847dc9ee9
iMac <levvaschenko@gmail.com>
2022-12-04 12:16:24 -0800

    exam work is over

---
+++ exam-work-1/src/container/App/App.tsx
@@ -0,0 +1,72 @@
+import { Button, TextField } from '@mui/material'
+import React, { useState } from 'react'
+import './App.scss'
+
+function App() {
+    const [count1, setCount1] = useState<number>(1)
+    const [count2, setCount2] = useState<number>(1)
+    const [count3, setCount3] = useState<number>(1)
+
+    const onIncrementClick1 = () =>
+        setCount1((prevState: number) => prevState + 1)
+
+    const onIncrementClick2 = () =>
+        setCount2((prevState: number) => prevState + 1)
+
+    const onIncrementClick3 = () =>
+        setCount3((prevState: number) => prevState + 1)
+    return (
+        <>
+            <div className="title">Перше завдання:</div>
+            <div className="buttons">
+                <Button
+                    variant="contained"
+                    size="small"
+                    onClick={() => onIncrementClick1()}
+                    className="button"
+                >
+                    Change count (
+                    <TextField
+                        size="small"
+                        value={count1}
+                        variant="outlined"
+                        className="input"
+                    />
+                    )
+                </Button>
+                <Button
+                    variant="contained"
+                    size="small"
+                    onClick={() => onIncrementClick2()}
+                    className="button"
+                >
+                    Change count (
+                    <TextField
+                        size="small"
+                        value={count2}
+                        variant="outlined"
+                        className="input"
+                    />
+                    )
+                </Button>
+                <Button
+                    variant="contained"
+                    size="small"
+                    onClick={() => onIncrementClick3()}
+                    className="button"
+                >
+                    Change count (
+                    <TextField
+                        size="small"
+                        value={count3}
+                        variant="outlined"
+                        className="input"
+                    />
+                    )
+                </Button>
+            </div>
+        </>
+    )
+}
+
+export default App
